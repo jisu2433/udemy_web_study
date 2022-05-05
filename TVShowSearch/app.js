ebd652b308e19caed376414f4e204587d8463d57
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-05-05 19:56:22 +0900

    Add : TVShowSearch

--- TVShowSearch/app.js
+++ TVShowSearch/app.js
@@ -3,10 +3,18 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
-  const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-  console.log(res.data);
-  console.log(res.data[0].show.image.medium);
-  const img = document.createElement('IMG');
-  img.src = res.data[0].show.image.medium;
-  document.body.append(img)
+  const config = { params: { q: searchTerm, isFunny: 'colt' }, Headers: {}}
+  const res = await axios.get(`https://api.tvmaze.com/search/shows?`,config);
+  makeImages(res.data);
+  form.elements.query.value = '';
 })
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement('IMG');
+      img.src = result.show.image.medium;
+      document.body.append(img)
+    }
+  } 
+}
