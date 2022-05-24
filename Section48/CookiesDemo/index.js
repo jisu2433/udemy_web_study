86cf1969a5467ec1d2ca890d42b879167fc8548f
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-05-24 15:55:21 +0900

    Add : Express의 세션과 플래시

---
+++ Section48/CookiesDemo/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+const session = require('express-session')
+
+const sessionOptions = { secret: 'thisisnotagoodsecret', resave: false, saveUninitualized: false}
+app.use(session(sessionOptions));
+
+app.get('/viewcount', (req, res) => {
+  if (req.session.count) {
+    req.session.count += 1;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`You have viewed this page ${req.session.count} times`)
+})
+
+app.get('/register', (req, res) => {
+  const { username = 'Anonymous' } = req.query;
+  req.session.username = username;
+  res.redirect('/greet')
+})
+
+app.get('/greet', (req, res) => {
+  const { username } = req.session;
+  res.send('Welcome back, ${username}')
+})
+
+app.listen(3000, () => {
+  console.log("SERVING!")
+})
